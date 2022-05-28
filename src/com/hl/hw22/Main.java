31ec592e8afd302bd391b6203309774c3a557153
Ярослава Ткаченко <zhulovayaroslava@gmail.com>
2022-05-28 22:11:37 +0300

    task22

---
+++ src/com/hl/hw22/Main.java
@@ -0,0 +1,9 @@
+package com.hl.hw22;
+
+public class Main {
+    public static void main(String[] args) {
+        TestRunner.start(TestClass.class);
+        System.out.println("___________");
+        TestRunner.start("TestClassNew");
+    }
+}
---
+++ src/com/hl/hw22/SuperTestClass.java
@@ -0,0 +1,9 @@
+package com.hl.hw22;
+
+public interface SuperTestClass {
+    static void start(String className) {
+    }
+
+    static void start(Class<?> testClass) {
+    }
+}
