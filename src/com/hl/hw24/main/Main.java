8a55e1ebcfe839fba3c0cee24709f49b2ab1f520
Ярослава Ткаченко <zhulovayaroslava@gmail.com>
2022-06-05 20:50:00 +0300

    task24

---
+++ src/com/hl/hw24/main/Main.java
@@ -0,0 +1,10 @@
+package com.hl.hw24.main;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Main {
+    public static void main(String[] args) throws ParseException, IOException {
+        new Scan().scan();
+    }
+}
