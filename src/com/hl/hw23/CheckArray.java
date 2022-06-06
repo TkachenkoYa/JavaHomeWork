0c9d285a775c54c345750eb932cdbe0629bf33dc
Ярослава Ткаченко <zhulovayaroslava@gmail.com>
2022-06-06 20:57:58 +0300

    task23

---
+++ src/com/hl/hw23/CheckArray.java
@@ -0,0 +1,4 @@
+package com.hl.hw23;
+
+public class CheckArray {
+}
---
+++ src/com/hl/hw23/CheckArrayTest.java
@@ -0,0 +1,18 @@
+package com.hl.hw23;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckArrayTest {
+    @Test
+    public void shouldReturnNewArrayWhenFourPresentSingle(){
+
+    }
+    @Test
+    public void shouldReturnNewArrayWhenFourPresentPlural(){
+
+    }
+    @Test
+    public void shouldReturnNewArrayWhenFourMissing(){
+
+    }
+}
